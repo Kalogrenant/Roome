e69ea4d8d771d6b4c461dc4d510a613c74ecbddf
Kalogrenant <79425513+Kalogrenant@users.noreply.github.com>
2021-02-21 16:11:35 -0800

    test map

---
+++ atlas_32x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="retro world" tilewidth="32" tileheight="32" tilecount="768" columns="48">
+ <image source="../Packages/Super_Retro_World_Interior_pack_week7/Package/atlas_32x.png" width="1536" height="512"/>
+</tileset>
